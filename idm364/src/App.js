8c7166240f1580b803a7f0c0b63146abb4631d99
jermestes <estesjeremy96@gmail.com>
2019-06-08 13:58:37 -0400

    structural change
    Moved the state load/db connect from the Nav component to App. (Getting the main event handler finally working later today/tomorrow)

--- idm364/src/App.js
+++ idm364/src/App.js
@@ -1,17 +1,104 @@
-import React from 'react';
-import './css/App.css';
+import React, { Component } from 'react';
 import Nav from './components/Nav';
+import Footer from './components/Footer'
 import base from './base';
+import firebase from 'firebase';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav></Nav>
-      <footer>
-        <p>©2019</p>
-      </footer>
+class App extends Component {
+  //Loading a blank state with all necessary state subtypes
+  state = {
+    items: [],
+    itemTypes: [],
+    orderNumItems: 0,
+    orderList: []
+  };
+
+  //Connecting app to Firebase DB, to sync state with it
+  componentDidMount() {
+    const dbRef = firebase.database().ref('items')
+    dbRef.on('value', (snapshot) => {
+        let items = snapshot.val();
+        let itemTypes = [];
+
+        //loop to get distinct item types for sorting in other components
+        for (let item in items) {
+            if (itemTypes.includes(items[item].type) != true)
+            itemTypes.push({
+                Type: items[item].type
+            })           
+        }
+
+        //ties app and database changes together
+        this.ref = base.syncState('items', {
+            context: this,
+            state: 'items'
+        });
+    })
+  }
+
+    //Event to sync any inventory form change to Firebase
+  /*inventoryChange = event => {
+    ...this.state.items, 
+    [event.currentTarget.name]: event.currentTarget.value;
+  }*/
+
+  updateItem = (key, updatedItem) => {
+    // console.log('update sign');
+    // 1. copy the current state
+    const signs = { ...this.state.items };
+    // 2. update that state key/value
+    signs[key] = updatedItem;
+    // 3. set the new copy to the component state
+    this.setState({ signs });
+  };
+
+  //Event to reset the menu and inventory to original values
+  resetStock = event => {
+    window.alert('test');
+  }
+
+  //Event to remove item from the Firebase
+  deleteFromInventory = event => {
+    window.alert('test');
+    const changedItem = this.state.items;
+  }
+
+  //Event to filter inventory forms (and possibly menu) to only show items of a specific type
+  itemTypeFilter = event => {
+    window.alert('test');
+    const changedItem = this.state.items;
+  }
+
+  //Event to add item(s) to the current order  
+  addToOrder = event => {
+    this.setState({orderNumItems: this.state.orderNumItems + 1})
+  }
+
+  //Event to remove item from the current order  
+  removeFromOrder = event => {
+    window.alert('test');
+  }
+
+  //Event for increase or decrease 
+  fieldIncrement = event => {
+    window.alert('test');
+  }
+
+  //No main div because that view is generated in Nav
+  render() {
+    return (
+      <div className="App">
+        <Nav appState={this.state} fieldIncrement={this.fieldIncrement.bind(this)} 
+          removeFromOrder={this.removeFromOrder.bind(this)} addToOrder={this.addToOrder.bind(this)}
+          updateItem={this.updateItem.bind(this)} itemTypeFilter={this.itemTypeFilter.bind(this)}
+          deleteFromInventory={this.deleteFromInventory.bind(this)}>
+        </Nav>
+        
+        <Footer></Footer>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- idm364/src/components/Nav.js
+++ idm364/src/components/Nav.js
@@ -1,82 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import base from '../base';
-import firebase from 'firebase';
 import Menu from './Menu';
 import Order from './Order';
 import Inventory from './Inventory';
 import logo from '../images/logo.svg';
 
-class Nav extends Component {
-  constructor() {
-    super();
-    this.state = {
-        items: [],
-        itemTypes: [],
-        orderNumItems: 0,
-        orderList: []
-    };
-  }
-
-  componentDidMount() {
-    const dbRef = firebase.database().ref('items')
-    dbRef.on('value', (snapshot) => {
-        let items = snapshot.val();
-        let itemTypes = [];
-
-        for (let item in items) {
-            if (itemTypes.includes(items[item].type) != true)
-            itemTypes.push({
-                Type: items[item].type
-            })           
-        }
-
-        this.ref = base.syncState('items', {
-            context: this,
-            state: 'items'
-        });
-    })
-  }
-
-  //Event to sync any inventory form change to Firebase
-  inventoryChange = event => {
-    window.alert('test');
-    const changedItem = this.state.items;
-  }
-
-  //Event to reset the menu and inventory to original values
-  resetStock = event => {
-    window.alert('test');
-  }
-
-  //Event to remove item from the Firebase
-  deleteFromInventory = event => {
-    window.alert('test');
-    const changedItem = this.state.items;
-  }
-
-  //Event to filter inventory forms (and possibly menu) to only show items of a specific type
-  itemTypeFilter = event => {
-    window.alert('test');
-    const changedItem = this.state.items;
-  }
-
-  //Event to add item(s) to the current order  
-  addToOrder = event => {
-    this.setState({orderNumItems: this.state.orderNumItems + 1})
-  }
-
-  //Event to remove item from the current order  
-  removeFromOrder = event => {
-    window.alert('test');
-  }
-
-  //Event for increase or decrease 
-  fieldIncrement = event => {
-    window.alert('test');
-  }
-
+class Nav extends Component {  
+  //Renders Header w/ image & nav-bar, followed by Switch that determines the main content
   render() {
     return (
       <div>
@@ -86,7 +17,7 @@ class Nav extends Component {
           <nav>
             <ul className="navbar-links">
               <Link to="/">Menu</Link>
-              <Link to="/Order">Order ({this.state.orderNumItems})</Link>
+              <Link to="/Order">Order ({this.props.appState.orderNumItems})</Link>
               <Link to="/Inventory">Inventory</Link>
             </ul>
           </nav> 
@@ -94,16 +25,13 @@ class Nav extends Component {
         
         <Switch>
           <Route path="/" exact render={ ()=> 
-          <Menu appState={this.state} addToOrder={this.addToOrder.bind(this)}/> } />
+          <Menu {...this.props} /> } />
 
           <Route path="/Order" render={ ()=> 
-          <Order appState={this.state} fieldIncrement={this.fieldIncrement.bind(this)} 
-          removeFromOrder={this.removeFromOrder.bind(this)}/>} />
+          <Order {...this.props} />} />
           
           <Route path="/Inventory" render={ ()=> 
-          <Inventory appState={this.state} inventoryChange={this.inventoryChange.bind(this)} 
-          fieldIncrement={this.fieldIncrement.bind(this)} itemTypeFilter={this.itemTypeFilter.bind(this)}
-          deleteFromInventory={this.deleteFromInventory.bind(this)}/>}/>
+          <Inventory {...this.props} />}/>
         </Switch>
       </div>
     );
