e0d1450d849401de9408fe8f341eb24eb8b6667e
jermestes <estesjeremy96@gmail.com>
2019-12-05 22:10:12 -0500

    removed build from gitignore for deployment

---
+++ build/precache-manifest.203accb3519759b61391a25875158527.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea11b014bbecb8b2193395830213cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff09c71ea699b5a77b2",
+    "url": "/static/css/main.30b30d45.chunk.css"
+  },
+  {
+    "revision": "442e6276cb41b36f5236",
+    "url": "/static/js/2.ce546d0b.chunk.js"
+  },
+  {
+    "revision": "aff09c71ea699b5a77b2",
+    "url": "/static/js/main.cbee93b7.chunk.js"
+  },
+  {
+    "revision": "1a21c707f27a34ebf35a",
+    "url": "/static/js/runtime-main.79f90b29.js"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/static/media/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "5f2a6299fadc0d3f9140900a09ff91e2",
+    "url": "/static/media/Syncopate-Regular.5f2a6299.ttf"
+  },
+  {
+    "revision": "f0f7c88c1d8ff4864efa1a956311dd9d",
+    "url": "/static/media/apple-pie.f0f7c88c.jpg"
+  },
+  {
+    "revision": "18aede702e14a89e1ee6eef55779d407",
+    "url": "/static/media/banana-muffin.18aede70.jpg"
+  },
+  {
+    "revision": "ca57457d3bf2335ccc994b4bfa52fb92",
+    "url": "/static/media/blueberry-muffin.ca57457d.jpg"
+  },
+  {
+    "revision": "586519291bb436bc7a1d2736696182b8",
+    "url": "/static/media/brownie-cookie.58651929.jpg"
+  },
+  {
+    "revision": "e7c1f77419520a8e8a2403d3dcd6176c",
+    "url": "/static/media/brownies.e7c1f774.jpg"
+  },
+  {
+    "revision": "9f8e7fa17775aa24576f4eaaf714d001",
+    "url": "/static/media/bw-milkshake.9f8e7fa1.jpg"
+  },
+  {
+    "revision": "a7654750896ccf696468dba9a56c2f53",
+    "url": "/static/media/cherry-pie.a7654750.jpg"
+  },
+  {
+    "revision": "5dfd17e19bfbf579271e903fa2cc94fa",
+    "url": "/static/media/choc-cupcake.5dfd17e1.jpg"
+  },
+  {
+    "revision": "b440e748694d1a65efd8fb32ea9771d4",
+    "url": "/static/media/chocolate-milkshake.b440e748.jpg"
+  },
+  {
+    "revision": "6da8cfb6c04ade687d53f84e9ad65541",
+    "url": "/static/media/chocolatechip-cookie.6da8cfb6.jpg"
+  },
+  {
+    "revision": "381327c2fc284bbefa03330d1c784afa",
+    "url": "/static/media/cookiecream-milkshake.381327c2.jpg"
+  },
+  {
+    "revision": "11e331ff306f5c5726d53a4f37a8b10b",
+    "url": "/static/media/logo.11e331ff.svg"
+  },
+  {
+    "revision": "5d48c65cd59dc60f1942ea26072d87fe",
+    "url": "/static/media/oreo-pie.5d48c65c.jpg"
+  },
+  {
+    "revision": "b4ed781d239700f095734173c978e94c",
+    "url": "/static/media/pumpkin-muffin.b4ed781d.jpg"
+  },
+  {
+    "revision": "3b583739a3a01bd0baaf311e5bcbc87d",
+    "url": "/static/media/pumpkin-pie.3b583739.jpg"
+  },
+  {
+    "revision": "a90085b1cb337b35a38cbc6827fa35cb",
+    "url": "/static/media/sugar-cookie.a90085b1.jpg"
+  },
+  {
+    "revision": "87451d9262c0c9f66e2077323f773ae0",
+    "url": "/static/media/vanilla-cupcake.87451d92.jpg"
+  },
+  {
+    "revision": "2cf013bd912440c0bf9479f8c3275180",
+    "url": "/static/media/vanilla-milkshake.2cf013bd.jpg"
+  }
+]);
